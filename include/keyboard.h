e6aae45a3418b0835583ded103ed23eceaf44b35
juliatoledo02 <julialuizatoledo@gmail.com>
2025-06-29 13:17:55 -0300

    movendo keyboard.h para include

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,1 +1,26 @@
-#include <iostream>
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <map>
+#include <string>
+
+enum class Color {
+    GREEN,
+    YELLOW,
+    GRAY
+};
+
+class Keyboard {
+private:
+    std::map<char, Color> keyColors;
+
+public:
+    Keyboard();
+    void setKeyColor(char letter, Color color);
+    Color getKeyColor(char letter) const;
+    void printKeyboard() const;
+    void resetKeyboard();
+};
+
+#endif // KEYBOARD_H
+
